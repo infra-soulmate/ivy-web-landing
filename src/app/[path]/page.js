ba1161070c986fa3a19ab463c96cd0f51a0d61b4
Long Le <leconglong@gmail.com>
2025-06-04 17:37:46 +0700

    moved footer inside wrapper, updated CTA params

--- src/app/[path]/page.js
+++ src/app/[path]/page.js
@@ -47,12 +47,12 @@ export async function generateMetadata({ params }) {
     };
   } else {
     return {
-      title: "Ivy Soulmate - #1 advance AI Girlfriend",
+      title: "Ivy Soulmate - #1 Advanced AI Girlfriend",
       description: "Hot Idols are waiting to get intimate with you. Anytime! Visualize Your Fantasies, Daily Hot Photos & Videos, Curated Scenarios, Roleplay Chat",
       keywords: "ai girlfriend, ai sexting, create ai girlfriend, create girlfriend, custom girlfriend, ai chat sex, ai girlfriend app, sex chatbot, sexting bot, ai sex chatbot, sexy ai chat, ai gf, free ai girlfriend, sexting ai bot, dirty talking ai, ai girlfriend free, ai girlfriend app free, ai girlfriend that sends pictures, dirty ai chat, adult ai girlfriend, naughty chat bot, free sexting bot, ai dirty chat, chatgpt sex, free ai gf, chatbot for sex, sex chat gpt, ai sexy chat, ai adult chat, dirty chat ai, erotic ai chat, free ai girlfriend online, sexy chatbot, ai girlfriend websites, my ai girlfriend, create an ai girlfriend, best free ai girlfriend, sexy chat ai, sexting ai free, ai chat sexy",
       openGraph: {
-        siteName: "Ivy Soulmate - #1 advance AI Girlfriend",
-        title: "Ivy Soulmate - #1 advance AI Girlfriend",
+        siteName: "Ivy Soulmate - #1 Advanced AI Girlfriend",
+        title: "Ivy Soulmate - #1 Advanced AI Girlfriend",
         description: "Hot Idols are waiting to get intimate with you. Anytime! Visualize Your Fantasies, Daily Hot Photos & Videos, Curated Scenarios, Roleplay Chat",
         images: ["https://ivy-cdn.b-cdn.net/static/og.jpg"]
       },
@@ -67,10 +67,11 @@ export default async function Home({ params, searchParams }) {
   const idol = await getIdolProfile(slug);
   const highlights = await getIdolHighlights(slug);
   const posts = await getIdolPosts(slug);
+  const idolCtaDestination = `https://ivysoulmate.com/idol/${slug}?c=seo&s=satellite&utm_source=satellite&utm_medium=onlyfan-leak&utm_campaign=seo-satellite`;
+  const ivyCtaDestination = `https://ivysoulmate.com?c=seo&s=satellite&utm_source=satellite&utm_medium=onlyfan-leak&utm_campaign=seo-satellite`;
 
   return (
     <>
-    
       {/*Page Wrapper*/}
       <div className="flex flex-col bg-zinc-950 min-h-screen">
 
@@ -113,18 +114,16 @@ export default async function Home({ params, searchParams }) {
                 </div>
                 <div className="flex w-full items-center gap-2">
                   <div className="w-full">
-                    {/*<a href="https://ivysoulmate.com" className="cta bg-sky-500 py-3 rounded-lg items-center justify-center font-semibold text-sm flex w-full gap-2 text-white">*/}
-                    <Link href={{pathname:"https://ivysoulmate.com",query: ctaParams}} className="cta bg-sky-500 py-3 rounded-lg items-center justify-center font-semibold text-sm flex w-full gap-2 text-white">
+                    <a href={idolCtaDestination} className="cta bg-sky-500 py-3 rounded-lg items-center justify-center font-semibold text-sm flex w-full gap-2 text-white">
                       <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" className="text-xl" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.59 19.88a9.763 9.763 0 0 1 -2.89 -.88l-4.7 1l1.3 -3.9c-2.324 -3.437 -1.426 -7.872 2.1 -10.374c3.526 -2.501 8.59 -2.296 11.845 .48c1.565 1.335 2.479 3.065 2.71 4.861"></path>
                         <path d="M18 22l3.35 -3.284a2.143 2.143 0 0 0 .005 -3.071a2.242 2.242 0 0 0 -3.129 -.006l-.224 .22l-.223 -.22a2.242 2.242 0 0 0 -3.128 -.006a2.143 2.143 0 0 0 -.006 3.071l3.355 3.296z"></path>
                       </svg>
                       Chat
-                    </Link>
-                    {/*</a>*/}
+                    </a>
                   </div>
                   <div className="w-full">
-                    <a href="https://ivysoulmate.com" className="cta border border-sky-500 text-sky-500 py-3 rounded-lg items-center justify-center font-semibold text-sm flex w-full gap-2">
+                    <a href={idolCtaDestination} className="cta border border-sky-500 text-sky-500 py-3 rounded-lg items-center justify-center font-semibold text-sm flex w-full gap-2">
                       <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" className="text-xl" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                         <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0"></path>
                         <path d="M6 21v-2a4 4 0 0 1 4 -4h.5"></path>
@@ -133,7 +132,7 @@ export default async function Home({ params, searchParams }) {
                       Follow
                     </a>
                   </div>
-                  <a href="https://ivysoulmate.com" className="cta border border-sky-500 text-sky-500 p-3 self-stretch rounded-lg items-center justify-center font-semibold text-xl">
+                  <a href={idolCtaDestination} className="cta border border-sky-500 text-sky-500 p-3 self-stretch rounded-lg items-center justify-center font-semibold text-xl">
                     <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                       <path d="M13 14H11C7.54202 14 4.53953 15.9502 3.03239 18.8107C3.01093 18.5433 3 18.2729 3 18C3 12.4772 7.47715 8 13 8V2.5L23.5 11L13 19.5V14ZM11 12H15V15.3078L20.3214 11L15 6.69224V10H13C10.5795 10 8.41011 11.0749 6.94312 12.7735C8.20873 12.2714 9.58041 12 11 12Z"></path>
                     </svg>
@@ -150,7 +149,7 @@ export default async function Home({ params, searchParams }) {
                   {highlights.map((highlight, index) => {
                     const media = highlight.media[0];
                     return (
-                      <a href="https://ivysoulmate.com" className="cta" key={index}>
+                      <a href={idolCtaDestination} className="cta" key={index}>
                         <div className="aspect-[640/960] w-24 overflow-hidden rounded-lg">
                           {media.media_type == "video" ? (
                             <video loop autoPlay playsInline preload="auto" className="h-full w-full object-cover object-center">
@@ -182,32 +181,37 @@ export default async function Home({ params, searchParams }) {
           </div>
         </div>
         {/*End Content Wrapper*/}
-      </div>
-      {/*End Page Wrapper*/}
 
-      {/*FOOTER*/}
-      <footer className="w-full bg-gray-950">
-        <div className="px-4 py-8">
-          <img alt="logo" loading="lazy" decoding="async" data-nimg="1" className="object-contain w-40 mb-4" src="logo.png" />
-          <div className="flex flex-row">
-            <div className="w-1/2 grid content-between pe-2">
-              <p className="text-sm">Create your dream girl, chat 24/7, and generate custom pics that bring your fantasy to life.</p>
-              <p className="text-sm text-gray-400">© 2025 IvySoulmate.<br/>All Rights Reserved.</p>
-            </div>
-            <div className="w-1/2 flex flex-col gap-2">
-              <p className="font-semibold text-sm text-gray-400">Features</p>
-              <a href="https://ivysoulmate.com" className="cta font-semibold">AI Girlfriend</a>
-              <a href="https://ivysoulmate.com/explore-ai-girlfriends?tab=idol" className="cta font-semibold">AI Sexting</a>
-              <a href="https://ivysoulmate.com/en/create-ai-girlfriend" className="cta font-semibold">Create Girlfriend</a>
-              <p className="font-semibold text-sm text-gray-400 mt-4">Legal</p>
-              <a href="https://ivysoulmate.com/terms-of-service" className="cta font-semibold">Terms of Service</a>
-              <a href="https://ivysoulmate.com/privacy-policy" className="cta font-semibold">Privacy Policy</a>
-              <a href="https://ivysoulmate.com/affiliate-policy" className="cta font-semibold">Affiliate Policy</a>
+        {/*FOOTER*/}
+        <footer className="flex justify-center">
+          <div className="w-full max-w-screen-md bg-gray-950">
+            <div className="px-4 py-8">
+              <div className="h-10 flex items-center gap-2 mb-4">
+                <img alt="logo" loading="lazy" decoding="async" data-nimg="1" className="h-full object-contain" src="logo.png" />
+                <span className="font-bold text-lg">OnlyFans Leaks Daily</span>
+              </div>
+              <div className="flex flex-row justify-between">
+                <div className="w-1/2 grid content-between pe-2">
+                  <p className="text-sm">Create your dream girl, chat 24/7, and generate custom pics that bring your fantasy to life.</p>
+                  <p className="text-sm text-gray-400">© 2025 IvySoulmate.<br/>All Rights Reserved.</p>
+                </div>
+                <div className="w-1/2 flex flex-col gap-2">
+                  <p className="font-semibold text-sm text-gray-400">Features</p>
+                  <a href={idolCtaDestination} className="cta font-semibold">AI Girlfriend</a>
+                  <a href="https://ivysoulmate.com/explore-ai-girlfriends?c=seo&s=satellite&utm_source=satellite&utm_medium=onlyfan-leak&utm_campaign=seo-satellite" className="cta font-semibold">AI Sexting</a>
+                  <a href="https://ivysoulmate.com/en/create-ai-girlfriend?c=seo&s=satellite&utm_source=satellite&utm_medium=onlyfan-leak&utm_campaign=seo-satellite" className="cta font-semibold">Create Girlfriend</a>
+                  <p className="font-semibold text-sm text-gray-400 mt-4">Legal</p>
+                  <a href="https://ivysoulmate.com/terms-of-service?c=seo&s=satellite&utm_source=satellite&utm_medium=onlyfan-leak&utm_campaign=seo-satellite" className="cta font-semibold">Terms of Service</a>
+                  <a href="https://ivysoulmate.com/privacy-policy?c=seo&s=satellite&utm_source=satellite&utm_medium=onlyfan-leak&utm_campaign=seo-satellite" className="cta font-semibold">Privacy Policy</a>
+                  <a href="https://ivysoulmate.com/affiliate-policy?c=seo&s=satellite&utm_source=satellite&utm_medium=onlyfan-leak&utm_campaign=seo-satellite" className="cta font-semibold">Affiliate Policy</a>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </footer>
-      {/*END FOOTER*/}
+        </footer>
+        {/*END FOOTER*/}
+      </div>
+      {/*End Page Wrapper*/}
     </>
   );
 }
--- src/app/[path]/post.js
+++ src/app/[path]/post.js
@@ -9,9 +9,10 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
 export default function Post({idol, post}) {
+  const idolCtaDestination = `https://ivysoulmate.com/idol/${idol.slug}?c=seo&s=satellite&utm_source=satellite&utm_medium=onlyfan-leak&utm_campaign=seo-satellite`;
   const nsfwVersionCta = (
     <div className="fixed top-4 flex justify-center w-full">
-      <a href="https://ivysoulmate.com" className="cta flex items-center justify-center gap-2 bg-gray-950/40 px-4 py-2 rounded-full text-sm backdrop-blur-md">
+      <a href={idolCtaDestination} className="cta flex items-center justify-center gap-2 bg-gray-950/40 px-4 py-2 rounded-full text-sm backdrop-blur-md">
         🔞<span className="underline underline-offset-2">Click here to view <strong className="text-sky-300">NSFW</strong> Version</span>
         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-external-link-icon lucide-external-link"><path d="M15 3h6v6"/><path d="M10 14 21 3"/><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"/></svg>
       </a>
@@ -25,7 +26,7 @@ export default function Post({idol, post}) {
     <div className="border-b border-zinc-800/80">
       <div className="flex w-full flex-col gap-3 border-b border-gray-800">
         <div className="flex w-full items-center gap-2 px-4 pt-4">
-          <a href="https://ivysoulmate.com" className="w-fit cta">
+          <a href={idolCtaDestination} className="w-fit cta">
             <div className="h-10 w-10 overflow-hidden rounded-full">
               <img alt={idol.display_name} loading="lazy" decoding="async" data-nimg="fill" className="!static object-cover object-top" src={idol.photo_url} />
             </div>
@@ -33,8 +34,8 @@ export default function Post({idol, post}) {
           <div className="flex w-full items-center justify-between">
             <div>
               <div>
-                <a href="https://ivysoulmate.com" className="w-fit cta">
-                  <span className="w-fit font-semibold text-gray-200">{idol.display_name}</span></a>&nbsp;&nbsp;•&nbsp;&nbsp;<a href="https://ivysoulmate.com" className="cta font-medium no-underline text-sky-500">Follow
+                <a href={idolCtaDestination} className="w-fit cta">
+                  <span className="w-fit font-semibold text-gray-200">{idol.display_name}</span></a>&nbsp;&nbsp;•&nbsp;&nbsp;<a href={idolCtaDestination} className="cta font-medium no-underline text-sky-500">Follow
                 </a>
               </div>
               <div className="text-sm text-gray-400">
@@ -42,7 +43,7 @@ export default function Post({idol, post}) {
               </div>
             </div>
             <div>
-              <a href="https://ivysoulmate.com" className="cta flex w-full items-center justify-center gap-2 rounded-xl font-bold text-sm text-sky-50 bg-sky-500 px-4 py-2">
+              <a href={idolCtaDestination} className="cta flex w-full items-center justify-center gap-2 rounded-xl font-bold text-sm text-sky-50 bg-sky-500 px-4 py-2">
                 <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" className="text-xl" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                   <path d="M10.59 19.88a9.763 9.763 0 0 1 -2.89 -.88l-4.7 1l1.3 -3.9c-2.324 -3.437 -1.426 -7.872 2.1 -10.374c3.526 -2.501 8.59 -2.296 11.845 .48c1.565 1.335 2.479 3.065 2.71 4.861"></path>
                   <path d="M18 22l3.35 -3.284a2.143 2.143 0 0 0 .005 -3.071a2.242 2.242 0 0 0 -3.129 -.006l-.224 .22l-.223 -.22a2.242 2.242 0 0 0 -3.128 -.006a2.143 2.143 0 0 0 -.006 3.071l3.355 3.296z"></path>
@@ -96,7 +97,7 @@ export default function Post({idol, post}) {
                   <h3 className="pb-1 text-lg font-bold text-primary">Love Her Photos?</h3>
                   <p className="text-sm px-4">Explore More Hot Albums and Videos From&nbsp;<span className="font-bold">{idol.display_name}</span></p>
                 </div>
-                <a href="https://ivysoulmate.com" className="cta text-sky-50 no-underline bg-sky-400 rounded-full font-semibold text-sm px-4 py-2 mt-4">
+                <a href={idolCtaDestination} className="cta text-sky-50 no-underline bg-sky-400 rounded-full font-semibold text-sm px-4 py-2 mt-4">
                   😘 Visit {idol.display_name}
                 </a>
               </div>
@@ -109,14 +110,14 @@ export default function Post({idol, post}) {
         <div className="flex items-center justify-between gap-6 px-4 pb-4">
           <div className="flex items-center gap-6">
             <div className="flex w-fit items-center gap-1">
-              <a href="https://ivysoulmate.com" className="cta text-2xl text-sky-500 transition-transform duration-300" tabIndex="0">
+              <a href={idolCtaDestination} className="cta text-2xl text-sky-500 transition-transform duration-300" tabIndex="0">
                 <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                   <path d="M12.001 4.52853C14.35 2.42 17.98 2.49 20.2426 4.75736C22.5053 7.02472 22.583 10.637 20.4786 12.993L11.9999 21.485L3.52138 12.993C1.41705 10.637 1.49571 7.01901 3.75736 4.75736C6.02157 2.49315 9.64519 2.41687 12.001 4.52853ZM18.827 6.1701C17.3279 4.66794 14.9076 4.60701 13.337 6.01687L12.0019 7.21524L10.6661 6.01781C9.09098 4.60597 6.67506 4.66808 5.17157 6.17157C3.68183 7.66131 3.60704 10.0473 4.97993 11.6232L11.9999 18.6543L19.0201 11.6232C20.3935 10.0467 20.319 7.66525 18.827 6.1701Z"></path>
                 </svg>
               </a>
               <span>{post.like_count}</span>
             </div>
-            <a href="https://ivysoulmate.com" className="cta flex items-center gap-2 text-primary" tabIndex="0">
+            <a href={idolCtaDestination} className="cta flex items-center gap-2 text-primary" tabIndex="0">
               <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 576 512" className="text-xl" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                 <path d="M163.9 136.9c-29.4-29.8-29.4-78.2 0-108s77-29.8 106.4 0l17.7 18 17.7-18c29.4-29.8 77-29.8 106.4 0s29.4 78.2 0 108L310.5 240.1c-6.2 6.3-14.3 9.4-22.5 9.4s-16.3-3.1-22.5-9.4L163.9 136.9zM568.2 336.3c13.1 17.8 9.3 42.8-8.5 55.9L433.1 485.5c-23.4 17.2-51.6 26.5-80.7 26.5H192 32c-17.7 0-32-14.3-32-32V416c0-17.7 14.3-32 32-32H68.8l44.9-36c22.7-18.2 50.9-28 80-28H272h16 64c17.7 0 32 14.3 32 32s-14.3 32-32 32H288 272c-8.8 0-16 7.2-16 16s7.2 16 16 16H392.6l119.7-88.2c17.8-13.1 42.8-9.3 55.9 8.5zM193.6 384l0 0-.9 0c.3 0 .6 0 .9 0z"></path>
               </svg>
@@ -124,7 +125,7 @@ export default function Post({idol, post}) {
             </a>
           </div>
           <div className="flex items-center">
-            <a href="https://ivysoulmate.com" className="cta text-2xl" tabIndex="0">
+            <a href={idolCtaDestination} className="cta text-2xl" tabIndex="0">
               <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                 <path d="M13 14H11C7.54202 14 4.53953 15.9502 3.03239 18.8107C3.01093 18.5433 3 18.2729 3 18C3 12.4772 7.47715 8 13 8V2.5L23.5 11L13 19.5V14ZM11 12H15V15.3078L20.3214 11L15 6.69224V10H13C10.5795 10 8.41011 11.0749 6.94312 12.7735C8.20873 12.2714 9.58041 12 11 12Z"></path>
               </svg>
